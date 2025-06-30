04f3fac1010577ca8c0258b1d059f35dcfbedaab
yash037 <yashww07@gmail.com>
2025-06-30 16:13:27 +0530

    Main starter file

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "../include/RedisServer.h"
+#include "../include/RedisDatabase.h"
+#include <thread>
+#include <chrono>
+
+// screw the lambda function syntax using normal
+void persistDatabase() {
+    while(true){
+        std::this_thread::sleep_for(std::chrono::seconds(300));
+        if(!RedisDatabase::getInstance().dump("dump.my_rcb")){
+            std::cerr<<"error dumping database\n";
+        }
+        else{
+            std::cout<<"database dumped to dump.my_rdb\n";
+        }
+    }
+}
+
+// In main()
+std::thread persistanceThread(persistDatabase);
+
+
+
+int main(int argc, char *argv[]){
+    int port = 6440;
+    if(argc >= 2) port = std::stoi(argv[1]);
+
+
+    //singleton pattern trololo
+    if(RedisDatabase::getInstance().load("dump.my_rdb")){
+        std::cout<<"database loaded from dump.my_rdb\n";
+    }
+    else{
+        std::cout<<"no dump file found database not loaded \n";
+    }
+
+    RedisServer server(port);
+
+    //dump database every 180 seconds
+    std::thread persistanceThread(persistDatabase);
+
+    //so dont join this thread later, this thread run independently forever (evern main returns)
+    persistanceThread.detach();
+
+    server.run();
+    return 0;
+}
